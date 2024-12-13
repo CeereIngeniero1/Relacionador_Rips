f8ffd422284514fea5fa76021354109b5927e388
Yeison Giraldo Ceere <yeisondeveloper@gmail.com>
2024-12-13 10:43:43 -0500

    correccion

--- server/routes/Asignar_RipsRoutes.js
+++ server/routes/Asignar_RipsRoutes.js
@@ -307,7 +307,7 @@ router.get('/Entidad/:Tipo', async (req, res) => {
 
         const request = new Request(
             `
-                SELECT        TOP (200) NombreCompletoPaciente, [Id Función], Función, DocumentoEntidad
+                SELECT         NombreCompletoPaciente, [Id Función], Función, DocumentoEntidad, IdTipoRips
                 FROM            [Cnsta Relacionador Entidades Rips]
                 WHERE        ([Id Función] = ${Tipo})
                 `,
